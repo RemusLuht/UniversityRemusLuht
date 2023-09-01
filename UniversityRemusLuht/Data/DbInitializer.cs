c608bae630adc8aa91b003cf2ca8e9b644f6d2be
RemusLuht <remusluht2005@gmail.com>
2023-09-01 15:04:50 +0300

    Added students, courses and enrollments

---
+++ UniversityRemusLuht/Data/DbInitializer.cs
@@ -0,0 +1,64 @@
+﻿using UniversityRemusLuht.Models;
+
+namespace UniversityRemusLuht.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Students.Any())
+            {
+                return;
+            }
+            var students = new Student[]
+            {
+                new Student(){FirstName="Kaarel-Martin",LastName="Noole",EnrollmentDate=DateTime.Now},
+                new Student(){FirstName="Palmi",LastName="Lahe",EnrollmentDate=DateTime.Parse("2021-09-01")},
+                new Student(){FirstName="Karl Umberto",LastName="Kats",EnrollmentDate=DateTime.Parse("2021-09-01")},
+                new Student(){FirstName="Kristjan Georg",LastName="Kessel",EnrollmentDate=DateTime.Parse("2022-09-01")},
+                new Student(){FirstName="Kenneth-Marcus",LastName="Aljas",EnrollmentDate=DateTime.Parse("2021-09-05")},
+                new Student(){FirstName="Henri",LastName="Jervson",EnrollmentDate=DateTime.Parse("2021-09-03")}
+            };
+            foreach (Student s in students) 
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+                new Course() {CourseID=1050,Title="Programmeerimine", Credits=160 },
+                new Course() {CourseID=6900,Title="Keemia", Credits=160 },
+                new Course() {CourseID=1420,Title="Matemaatika", Credits=160 },
+                new Course() {CourseID=6666,Title="Testimine", Credits=160 },
+                new Course() {CourseID=1234,Title="Riigikaitse", Credits=160 }
+            };
+            foreach(Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+                new Enrollment{StudentID=1, CourseID=1050,Grade=Grade.A},
+                new Enrollment{StudentID=1, CourseID=6900,Grade=Grade.B},
+                new Enrollment{StudentID=2, CourseID=1420,Grade=Grade.B},
+                new Enrollment{StudentID=2, CourseID=6666,Grade=Grade.C},
+                new Enrollment{StudentID=3, CourseID=6666,Grade=Grade.C},
+                new Enrollment{StudentID=3, CourseID=1234,Grade=Grade.A},
+                new Enrollment{StudentID=4, CourseID=6666,Grade=Grade.F},
+                new Enrollment{StudentID=4, CourseID=6900,Grade=Grade.A},
+                new Enrollment{StudentID=5, CourseID=1050,Grade=Grade.F},
+                new Enrollment{StudentID=5, CourseID=6666,Grade=Grade.C}
+            };
+            foreach (Enrollment e in enrollments)
+            {
+                context.Enrollments.Add(e);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- UniversityRemusLuht/Program.cs
+++ UniversityRemusLuht/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityRemusLuht.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
