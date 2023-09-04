3fd06cdfda52a5ffa7b8ea166e4c16632b0864d6
RemusLuht <remusluht2005@gmail.com>
2023-09-04 16:41:17 +0300

    fixed errors

--- UniversityRemusLuht/Program.cs
+++ UniversityRemusLuht/Program.cs
@@ -6,9 +6,6 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        var host = CreateHostBuilder(args).Build();
-        CreateDbIfNotExists(host);
-        host.Run();
 
         var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +13,9 @@ public class Program
         builder.Services.AddControllersWithViews();
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
         builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
         var app = builder.Build();
+
+        CreateDbIfNotExists(app);
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
