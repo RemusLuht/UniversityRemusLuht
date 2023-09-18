e13773142dd9d0a4c4f4262b0e3dc103760c19de
RemusLuht <remusluht2005@gmail.com>
2023-09-18 16:40:00 +0300

    instructor views2

---
+++ UniversityRemusLuht/Models/AssignedCourseData.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityRemusLuht.Models
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
